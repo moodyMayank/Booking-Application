b52cdf5a46be247e8f78a5ffbca1fad764aca75f
Mayank <mayankbudhiraja88@gmail.com>
2023-02-01 23:08:11 +0530

    completed Banner Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import Banner from "./components/Banner";
 
 const App = () => {
   return (
-    <>
-      {/* Header */}
+    <div>
+      {/* Header Section*/}
       <Header />
-      {/* Banner */}
+      {/* Banner Section*/}
       <Banner />
-    </>
+
+      <main className="">
+        {/* Explore Section */}
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+        </section>
+      </main>
+    </div>
   );
 };
 
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,17 @@
 const Banner = () => {
   return (
-    <div className="relative h-[300px] sm:h-[400px]">
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
       <img
         className="object-fill h-full w-full"
         src="https://links.papareact.com/0fm"
         alt="Banner Image"
       />
+      <div className="absolute top-1/2 w-full text-center">
+        <p className="text-sm sm:text-lg">Not sure where to go? Perfect</p>
+        <button className="text-purple-500 bg-white my-3 px-10 py-4 shadow-md rounded-full font-bold hover:shadow-xl active:scale-90 transition duration-150">
+          I'm flexible
+        </button>
+      </div>
     </div>
   );
 };
